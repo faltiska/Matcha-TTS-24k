12f4b3b209d6e7462af139600bdab9ca8d586e67
Faltiska <alfred.faltiska@outlook.com>
2026-01-21 11:29:10 +0200

    simplified the espeak initialization code

--- matcha/text/phonemizers.py
+++ matcha/text/phonemizers.py
@@ -16,77 +16,14 @@ logger.setLevel(logging.ERROR) # eSpeak is very verbose
 
 # Initializing the phonemizer globally significantly improves the speed.
 phonemizers = {}
-
-phonemizers["en-us"] = phonemizer.backend.EspeakBackend(
-    language="en-us",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["en-gb"] = phonemizer.backend.EspeakBackend(
-    language="en-gb",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["ro"] = phonemizer.backend.EspeakBackend(
-    language="ro",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["fr-fr"] = phonemizer.backend.EspeakBackend(
-    language="fr-fr",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["de"] = phonemizer.backend.EspeakBackend(
-    language="de",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["es"] = phonemizer.backend.EspeakBackend(
-    language="es",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["pt"] = phonemizer.backend.EspeakBackend(
-    language="pt",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["it"] = phonemizer.backend.EspeakBackend(
-    language="it",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["ja"] = phonemizer.backend.EspeakBackend(
-    language="ja",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
-phonemizers["he"] = phonemizer.backend.EspeakBackend(
-    language="he",
-    preserve_punctuation=True,
-    with_stress=True,
-    language_switch="remove-flags",
-    logger=logger,
-)
+for lang in ["en-us", "en-gb", "ro", "fr-fr", "de", "es", "pt", "it", "ja", "he"]:
+    phonemizers[lang] = phonemizer.backend.EspeakBackend(
+        language=lang,
+        preserve_punctuation=True,
+        with_stress=True,
+        language_switch="remove-flags",
+        logger=logger,
+    )
 
 # eSpeak does not remove the dot after expanding some abbreviations, like "Dr.", making the TTS pause a bit.
 # Search for $dot here: https://github.com/espeak-ng/espeak-ng/blob/master/dictsource/en_list
