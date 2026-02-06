Changed duration loss computation from L2 to L1 too, which fixed duration learning.
Reformatted ** instructions to have consistency. I needed to search for x to the power of 2, but could not find all instances because some had spaces, some didn't.

matcha/models/components/text_encoder.py
@@ -352,7 +352,7 @@ class TextEncoder(nn.Module):
         self.n_spks = n_spks
 
         self.emb = torch.nn.Embedding(n_vocab, self.n_channels)
-        torch.nn.init.normal_(self.emb.weight, 0.0, self.n_channels**-0.5)
+        torch.nn.init.normal_(self.emb.weight, 0.0, self.n_channels ** -0.5)
 
         if encoder_params.prenet:
             self.prenet = ConvReluNorm(