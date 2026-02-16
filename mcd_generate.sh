d92d425354ba8871491bc3913731dd910a8dbd59
Faltiska <alfred.faltiska@outlook.com>
2026-02-16 19:52:36 +0200

    Updated the MCD scripts

--- mcd_generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CHECKPOINT_PATH="logs/train/corpus-small-24k/runs/2026-02-16_11-22-14/checkpoints/saved/checkpoint_epoch=529.ckpt"
-
-python -m matcha.cli --text "There is a strong chance it will happen once more." --vocoder vocos --checkpoint_path "$CHECKPOINT_PATH" --spk "0,1,2,6" --language en-us
-python -m matcha.cli --text "There is a strong chance it will happen once more." --vocoder vocos --checkpoint_path "$CHECKPOINT_PATH" --spk "3,4,5" --language en-gb
-
-python -m matcha.cli --text "Elles doivent simplement être précisées par décret." --vocoder vocos --checkpoint_path "$CHECKPOINT_PATH" --spk "8,9" --language fr-fr
-python -m matcha.cli --text "Geologul analizează geoda găsită în groapa adâncă din grădină." --vocoder vocos --checkpoint_path "$CHECKPOINT_PATH" --spk "7" --language ro
