e0b6bc79dd9df58a46bb7fbb41992fef6127b2e4
Faltiska <alfred.faltiska@outlook.com>
2026-02-20 19:50:38 +0200

    Don't enforce a minimum duration for phonemes inside generate_path, so I can do it before calling it.

--- matcha/utils/model.py
+++ matcha/utils/model.py
@@ -29,11 +29,6 @@ def convert_pad_shape(pad_shape):
 def generate_path(duration, mask):
     b, t_x, t_y = mask.shape
 
-    # Ensure all durations are at least 0.5 to prevent zero-length phonemes after rounding
-    # For example these durations: [1.1, 0.3, 1.8] may result in a cumsum of [1.1, 1.4, 3.2]
-    # which, when rounded up will be [1, 1, 3] meaning the second phoneme will start over the first one
-    duration = torch.clamp_min(duration, 1.0)
-
     # Duration could have fractional numbers during inference, because there's an exp(duration) there.
     # During training, It will only have integers, because they are coming from MAS.
     cum_duration = torch.cumsum(duration, 1).round()
