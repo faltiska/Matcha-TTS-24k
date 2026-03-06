3c6f72fabf6ef80bf08adad363fe0ac5bf24bb2a
Faltiska <alfred.faltiska@outlook.com>
2026-03-06 18:24:37 +0200

    Removed the code related to precomputed durations.

--- matcha/models/matcha_tts.py
+++ matcha/models/matcha_tts.py
@@ -2,7 +2,6 @@ import math
 import torch
 
 import logging
-from matcha import utils
 from matcha.models.baselightningmodule import BaseLightningClass
 from matcha.models.components.flow_matching import CFM
 from matcha.models.components.text_encoder import TextEncoder
@@ -22,35 +21,42 @@ class MatchaTTS(BaseLightningClass):  # 🍵
         self,
         n_vocab,
         n_spks,
-        spk_emb_dim,
         n_feats,
         encoder,
         decoder,
         cfm,
         data_statistics,
+        spk_emb_dim=None,
+        spk_emb_dim_enc=None,
+        spk_emb_dim_dur=None,
+        spk_emb_dim_dec=None,
         optimizer=None, # parameter required by BaseLightningClass
         scheduler=None, # parameter required by BaseLightningClass
         prior_loss=True,
-        use_precomputed_durations=False,
         plot_mel_on_validation_end=False,
     ):
         super().__init__()
 
         self.save_hyperparameters(logger=False)
 
+        spk_emb_dim_enc = spk_emb_dim_enc or spk_emb_dim
+        spk_emb_dim_dur = spk_emb_dim_dur or spk_emb_dim
+        spk_emb_dim_dec = spk_emb_dim_dec or spk_emb_dim
+
         self.n_vocab = n_vocab
         self.n_spks = n_spks
-        self.spk_emb_dim = spk_emb_dim
+        self.spk_emb_dim_enc = spk_emb_dim_enc
+        self.spk_emb_dim_dur = spk_emb_dim_dur
+        self.spk_emb_dim_dec = spk_emb_dim_dec
         self.n_feats = n_feats
         self.prior_loss = prior_loss
-        self.use_precomputed_durations = use_precomputed_durations
         self.plot_mel_on_validation_end = plot_mel_on_validation_end
         self.mas_const = -0.5 * LOG_2_PI * n_feats
 
         if n_spks > 1:
-            self.encoder_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim)
-            self.duration_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim)
-            self.decoder_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim)
+            self.encoder_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim_enc)
+            self.duration_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim_dur)
+            self.decoder_speaker_embeddings = torch.nn.Embedding(n_spks, spk_emb_dim_dec)
 
         self.encoder = TextEncoder(
             encoder.encoder_type,
@@ -58,7 +64,8 @@ class MatchaTTS(BaseLightningClass):  # 🍵
             encoder.duration_predictor_params,
             n_vocab,
             n_spks,
-            spk_emb_dim,
+            spk_emb_dim_enc,
+            spk_emb_dim_dur,
         )
 
         self.decoder = CFM(
@@ -67,12 +74,12 @@ class MatchaTTS(BaseLightningClass):  # 🍵
             cfm_params=cfm,
             decoder_params=decoder,
             n_spks=n_spks,
-            spk_emb_dim=spk_emb_dim,
+            spk_emb_dim=spk_emb_dim_dec,
         )
 
         self.update_data_statistics(data_statistics)
 
-    def forward(self, x, x_lengths, y, y_lengths, spks=None, durations=None):
+    def forward(self, x, x_lengths, y, y_lengths, spks=None):
         """
         Computes 3 losses:
             1. duration loss: loss between predicted token durations and those extracted by Monotonic Alignment Search (MAS).
@@ -105,30 +112,29 @@ class MatchaTTS(BaseLightningClass):  # 🍵
         y_mask = sequence_mask(y_lengths, y_max_length).unsqueeze(1).to(x_mask)
         attn_mask = x_mask.unsqueeze(-1) * y_mask.unsqueeze(2)
 
-        if self.use_precomputed_durations:
-            attn = generate_path(durations.squeeze(1), attn_mask.squeeze(1))
-        else:
-            # Use MAS to find most likely alignment `attn` between text and mel-spectrogram
-            with torch.no_grad():
-                # This computes the distance between every text token and ground truth mel frame 
-                # using a  Gaussian log-likelihood formula 
-                factor = -0.5 * torch.ones(mu_x.shape, dtype=mu_x.dtype, device=mu_x.device)
-                y_square = torch.matmul(factor.transpose(1, 2), y ** 2)
-                # Original code was:
-                #   y_mu_double = torch.matmul(2.0 * (factor * mu_x).transpose(1, 2), y)
-                # But (2.0 * factor * mu_x) is useless, because factor = -0.5 * tensor.ones
-                # I've replaced it with just -mu_x
-                y_mu_double = torch.matmul(-mu_x.transpose(1, 2), y)
-                mu_square = torch.sum(factor * (mu_x ** 2), 1).unsqueeze(-1)
-                log_prior = y_square - y_mu_double + mu_square + self.mas_const
+        # Use MAS to find most likely alignment `attn` between text and mel-spectrogram
+        with torch.no_grad():
+            # This computes the distance between every text token and ground truth mel frame 
+            # using a  Gaussian log-likelihood formula 
+            factor = -0.5 * torch.ones(mu_x.shape, dtype=mu_x.dtype, device=mu_x.device)
+            y_square = torch.matmul(factor.transpose(1, 2), y ** 2)
+            # Original code was:
+            #   y_mu_double = torch.matmul(2.0 * (factor * mu_x).transpose(1, 2), y)
+            # But (2.0 * factor * mu_x) is useless, because factor = -0.5 * tensor.ones
+            # I've replaced it with just -mu_x
+            y_mu_double = torch.matmul(-mu_x.transpose(1, 2), y)
+            mu_square = torch.sum(factor * (mu_x ** 2), 1).unsqueeze(-1)
+            log_prior = y_square - y_mu_double + mu_square + self.mas_const
 
-                attn = maximum_path_gpu(log_prior, attn_mask.squeeze(1).to(torch.int32), log_prior.dtype)
+            attn = maximum_path_gpu(log_prior, attn_mask.squeeze(1).to(torch.int32), log_prior.dtype)
 
         # torch.sum(attn.unsqueeze(1), -1)) says how many mel frames each text token aligns to
         # x_mask has 1s for valid text tokens, 0s for padding positions, to ensure loss is only calculated on 
         # valid tokens, preventing attention to padding.
-        logw_ = torch.log(1e-8 + torch.sum(attn.unsqueeze(1), -1)) * x_mask
-        # logw - log-scaled durations calculated by the TextEncoder's duration predictor
+        mas_durations = torch.sum(attn.unsqueeze(1), -1).squeeze(1)  # (B, T_text)
+        logw_ = torch.log(1e-8 + mas_durations.unsqueeze(1)) * x_mask
+
+        # logw - log-scaled durations from the Duration Predictor
         # logw_ - log-scaled durations calculated the Monotonic Alignment Search algorithm. 
         dur_loss = duration_loss(logw, logw_, x_lengths)
 
